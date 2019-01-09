5c4fdf75cef6a897a911b0786bccf6958f247452
Yoichi Toyota <y.toyota@xtone.co.jp>
2019-01-09 11:54:58 +0900

    Fix routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :storage, only: %(index create)
+  resources :storages, only: %(index create)
 end
